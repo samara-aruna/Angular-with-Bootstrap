f2e524260c4237ae6912245b20161029f15403c6
samara-aruna <asamarasinghe154@gmail.com>
2024-08-06 10:57:24 +0530

    Ann BMI calculter to the web page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent 
+{
+  public weight! : number;
+  public height!:number;
+  public bmi!:number;
+
+  calculate()
+  {
+    this.bmi = this.weight/(Math.sqrt(this.height/100));
+
+    if(this.bmi < 18.5)
+    {
+      alert("You are in UnderWeight range");
+    }
+    else if(this.bmi >= 18.5 && this.bmi<=24.9)
+    {
+      alert("You are in Healthy Weight Range");
+    }
+    else if(this.bmi >= 25 && this.bmi<=29.9)
+    {
+      alert("You are in OverWeight range");
+    }
+    else
+    {
+      alert("You are in Obesity Weight Range");
+    }
+  }
 
 }
